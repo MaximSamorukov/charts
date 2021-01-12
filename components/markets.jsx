455216c64348a557b526971b7150a25d5e51cf87
Maxim Samorukov <maxim.samorukov@gmail.com>
2021-01-12 21:19:35 +0300

    updated bottons

--- components/markets.jsx
+++ components/markets.jsx
@@ -13,13 +13,13 @@ export default class Markets extends React.Component {
   render() {
     const { name } = this.props;
     return (
-      <div className={`border markets-container d-flex col-12 p-1`}>
-        <div onClick={this.onclick} className="btn-group container-fluid">
-          <button className="btn btn-primary">NYSE</button>
-          <button className="btn btn-primary">NYSE ARCA</button>
-          <button className="btn btn-primary">NASDAQ</button>
-          <button className="btn btn-primary">NYSE MKT</button>
-          <button className="btn btn-primary">BATS</button>
+      <div className={`border markets-container d-flex col-12`}>
+        <div onClick={this.onclick} className="btn-group container-fluid d-flex justify-content-around flex-nowrap p-0 m-0">
+          <button className="btn btn-primary p-1 text-center col-2">NYSE</button>
+          <button className="btn btn-primary p-1 text-center col-3">NYSE ARCA</button>
+          <button className="btn btn-primary p-1 text-center col-3">NASDAQ</button>
+          <button className="btn btn-primary p-1 text-center col-2">NYSE MKT</button>
+          <button className="btn btn-primary p-1 text-center col-2">BATS</button>
         </div>
       </div>
     )
