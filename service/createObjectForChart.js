418a32cfa625ea12caddf1ee2944fda844456cbc
Maxim Samorukov <maxim.samorukov@gmail.com>
2021-01-18 18:40:53 +0300

    updated charts

--- service/createObjectForChart.js
+++ service/createObjectForChart.js
@@ -24,7 +24,7 @@ export default ({ item, store: { interval, market, ticket, company, amount, ifAm
     yaxis: {
       tooltip: {
         enabled: true,
-      }
+      },
     },
     series: [{
       data
@@ -45,7 +45,7 @@ export default ({ item, store: { interval, market, ticket, company, amount, ifAm
         }
       },
       padding: {
-        top: 5,
+        top: 0,
         right: 0,
         bottom: 0,
         left: 0
